c7b04e8373a3f431f12e0c788b16c7a145b84f38
stephanie133.tait@gmail.com <stephanie133.tait@gmail.com>
2019-09-08 19:10:47 -0400

    adding and removing todo items

---
+++ src/components/TodoList.vue
@@ -0,0 +1,94 @@
+<template>
+    <div>
+        <input 
+            type="text" 
+            class="todo-input"
+            placeholder="what needs to be done"
+            v-model="newTodo" 
+            @keyup.enter="addTodo">
+            
+        <div v-for="(todo, index) in todos" :key="todo.id" class="todo-item">
+            <div> 
+                {{ todo.title }}
+            </div>
+            <div class="remove-item" @click="removeTodo(index)">
+                &times; 
+            </div>
+        </div>    
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'todo-list',
+    data () {
+        return {
+            newTodo: '',
+            idForTodo: 3,
+            todos: [
+                {
+                    'id': 1,
+                    'title': 'Finish Vue Screencast',
+                    'completed': false,
+                },
+                {
+                    'id': 2,
+                    'title': 'Take over world',
+                    'completed': false,
+                },
+            ]
+        }
+    },
+    methods: {
+        addTodo(event) {
+            if(this.newTodo.trim().length == 0) {
+                alert('sorry no blanks allowed');
+                return;
+            }
+
+            this.todos.push({
+                id: this.idForTodo,
+                title: this.newTodo,
+                completed: false
+            })
+            
+            this.newTodo = '';
+            this.idForTodo++;
+        },
+        removeTodo(index) {
+            this.todos.splice(index, 1);
+        }
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+    .todo-input {
+        width: 100%;
+        padding: 10px 18px;
+        font-size: 18px;
+        margin-bottom: 18px;
+
+        &:focus {
+            ooutline: 0;
+        }
+    }
+
+    .todo-item {
+        margin-bottom: 12px;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+
+
+   .remove-item {
+       cursor: pointer;
+       margin-left: 14px;
+       &:hover {
+           color: black;
+       }
+   }
+        
+</style>
