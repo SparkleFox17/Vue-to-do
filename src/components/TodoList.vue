0dc7224318fe6ec980edffa86006b69ac65d9704
stephanie133.tait@gmail.com <stephanie133.tait@gmail.com>
2019-09-08 20:10:58 -0400

    adding edit function

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -7,9 +7,27 @@
             v-model="newTodo" 
             @keyup.enter="addTodo">
             
-        <div v-for="(todo, index) in todos" :key="todo.id" class="todo-item">
-            <div> 
-                {{ todo.title }}
+        <div 
+            v-for="(todo, index) in todos" 
+            :key="todo.id" 
+            class="todo-item">
+            <div class="todo-item-left">
+                <div 
+                    v-if="!todo.editing" 
+                    @dblclick="editTodo(todo)" 
+                    class="todo-item-label">
+                    {{ todo.title }}
+                </div>
+                <input 
+                    v-else 
+                    class="todo-item-edit" 
+                    type="text" 
+                    v-model="todo.title" 
+                    @blur="doneEdit(todo)"
+                    @keyup.enter="doneEdit(todo)"
+                    v-focus  
+                    @keyup.esc="cancelEdit(todo)">
+
             </div>
             <div class="remove-item" @click="removeTodo(index)">
                 &times; 
@@ -25,20 +43,32 @@ export default {
         return {
             newTodo: '',
             idForTodo: 3,
+            beforeEditCache: '',
             todos: [
                 {
                     'id': 1,
                     'title': 'Finish Vue Screencast',
                     'completed': false,
+                    'editing': false
                 },
                 {
                     'id': 2,
                     'title': 'Take over world',
                     'completed': false,
+                    'editing': false
                 },
             ]
         }
     },
+
+    directives: {
+        focus: {
+            inserted: function (el) {
+                el.focus();
+            }
+        }
+    },
+
     methods: {
         addTodo(event) {
             if(this.newTodo.trim().length == 0) {
@@ -49,12 +79,31 @@ export default {
             this.todos.push({
                 id: this.idForTodo,
                 title: this.newTodo,
-                completed: false
+                completed: false,
+                editing: false
             })
             
             this.newTodo = '';
             this.idForTodo++;
         },
+
+        editTodo(todo) {
+            todo.editing  = true;
+            this.beforeEditCache = todo.title;
+        },
+
+        doneEdit(todo) {
+            if(todo.title.trim() == '') {
+                todo.title = this.beforeEditCache;
+            }
+            todo.editing = false;
+        },
+
+        cancelEdit(todo) {
+            todo.title = this.beforeEditCache;
+            todo.editing = false;
+        },
+
         removeTodo(index) {
             this.todos.splice(index, 1);
         }
@@ -89,6 +138,31 @@ export default {
        &:hover {
            color: black;
        }
+   }
+
+   .todo-item-left {
+       display: flex;
+       align-items: center;
+   }
+
+   .todo-list-label {
+       padding: 10px;
+       border: 1px solid white;
+       margin-left: 12px;
+   }
+
+   .todo-item-edit {
+       font-size: 24px;
+       color: #2c3e50;
+       margin-left: 12px;
+       width: 100%;
+       padding: 10px;
+       border: 1px solid #ccc;
+       font-family: 'Avenir', Helvetica, Arial, Sans-serif;
+    
+        &:focus {
+            outline: none;   
+        }
    }
         
 </style>
